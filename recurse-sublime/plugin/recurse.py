12dd385ed471d810b233164ea9c597f5e31ac6a8
Knut Andreas Ruud <kr@vxg.com>
2016-08-12 23:02:05 +0200

    Adding a dummy sublime text plugin, which will be used later on to fetch data from a host to a recurse code file

--- recurse-sublime/plugin/recurse.py
+++ recurse-sublime/plugin/recurse.py
@@ -1,1 +1,6 @@
-/Users/upland/Library/Application Support/Sublime Text 3/Packages/User/recurse.py
+import sublime, sublime_plugin
+
+class RecurseCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		for pos in self.view.sel():
+			self.view.insert(edit, pos.begin(), "Hello there!")
