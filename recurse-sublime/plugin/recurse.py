39c657fa90c7c3499e28234692949278ad529b81
Knut Andreas Ruud <kr@vxg.com>
2016-09-21 22:51:02 +0200

    Sublime plugin now working

--- recurse-sublime/plugin/recurse.py
+++ recurse-sublime/plugin/recurse.py
@@ -1,6 +1,37 @@
 import sublime, sublime_plugin
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "osc4py3-1.0.1"))
+
+from osc4py3.as_eventloop import *
+from osc4py3 import oscbuildparse
+import time
+
+class A(object): pass
+
+a = A()
+a.value = ""
+
+def handlerfunction(args):
+	a.value = "".join(args)
+
+def plugin_loaded():
+	osc_startup()
+	osc_udp_client("127.0.0.1", 8009, "sender")
+	osc_udp_server("127.0.0.1", 8008, "receiver")
+	osc_method("/recurse/intervals", handlerfunction)
+
+def plugin_unloaded():
+	osc_terminate()
 
 class RecurseCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		msg = oscbuildparse.OSCMessage("/recurse/getintervals", None, [])
+		osc_send(msg, "sender")
+
+		targettime = time.time() + 1
+		while time.time() < targettime and a.value == "":
+			osc_process()
+
 		for pos in self.view.sel():
-			self.view.insert(edit, pos.begin(), "Hello there!")
+			self.view.insert(edit, pos.begin(), a.value)
