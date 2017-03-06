82baf6febf0e9647435ed6ebe94329e9155bacec
Knut Andreas Ruud <kr@upland.no>
2017-03-06 22:58:34 +0100

    Range now handles high to low values properly

--- recurse-app/app/src/function/operator/Range.ts
+++ recurse-app/app/src/function/operator/Range.ts
@@ -32,12 +32,14 @@ export class Range implements INode {
         if (start.valueType !== start.valueType) {
             console.log(`WARN: Range expected arguments of same type, but received ${ValueType[start.valueType]} and ${ValueType[end.valueType]}`);
         }
-        // todo: should support ranges from high to low as well as low to high
         if (startValue > endValue) {
-            [startValue, endValue] = [endValue, startValue];
-        }
-        for (let i = startValue; i <= endValue; i++) {
-            results.push({value: i, valueType: start.valueType});
+            for (let i = startValue; i >= endValue; i--) {
+                results.push({value: i, valueType: start.valueType});
+            }
+        } else {
+            for (let i = startValue; i <= endValue; i++) {
+                results.push({value: i, valueType: start.valueType});
+            }
         }
         return results;
     }
